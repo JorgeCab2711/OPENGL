607d00ff1ad9a865eda266d6b46a1569f109dafa
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-11-15 18:33:35 -0600

    pre transf

--- gl.py
+++ gl.py
@@ -15,11 +15,15 @@ class Renderer:
         self.clock = pg.time.Clock()
         # initialize OpenGL
         glClearColor(0.1, 0.2, 0.2, 1)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         self.shader = self.createShader(
             "shaders/vertex.txt", "shaders/fragment.txt"
         )
         glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
         self.triangle = Triangle()
+        self.wood_texture = Material("Textures\wood.png")
         self.mainLoop()
 
     def mainLoop(self):
@@ -38,6 +42,7 @@ class Renderer:
             glClear(GL_COLOR_BUFFER_BIT)
             pg.display.flip()
             glUseProgram(self.shader)
+            self.wood_texture.use()
             glBindVertexArray(self.triangle.vao)
             glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
 
@@ -49,6 +54,7 @@ class Renderer:
 
     def quit(self):
         self.triangle.destroy()
+        self.wood_texture.destroy()
         glDeleteProgram(self.shader)
         pg.quit()
 
@@ -70,9 +76,9 @@ class Triangle:
         # Positions
         # x , y , z , s , t , nx ,ny ,nz
         self.vertices = (
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-            0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-            0.0, 0.5, 0.0, 0.0, 0.0, 1.0,
+            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 0.1,
+            0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+            0.0, 0.5, 0.0, 0.0, 0.0, 1.0, 0.5, 0.0
         )
         self.vertices = np.array(self.vertices, dtype=np.float32)
         self.vertex_count = 3
@@ -82,15 +88,46 @@ class Triangle:
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
         glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes,
                      self.vertices, GL_STATIC_DRAW)
+        # Attributes
+        # at1 - position
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                              32, ctypes.c_void_p(0))
+        # at2 - color
         glEnableVertexAttribArray(1)
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
-                              24, ctypes.c_void_p(12))
+                              32, ctypes.c_void_p(12))
+        # at3 - Texture Coordinates
+        glEnableVertexAttribArray(2)
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE,
+                              32, ctypes.c_void_p(24))
 
     def destroy(self):
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteVertexArrays(1, (self.vbo,))
+
+
+class Material:
+    def __init__(self, filepath):
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        image = pg.image.load(filepath).convert_alpha()
+        image_width, image_height = image.get_rect().size
+        image_data = pg.image.tostring(image, "RGBA")
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width,
+                     image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def use(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+    def destroy(self):
+        glDeleteTextures(1, (self.texture,))
 
 
 if __name__ == "__main__":
