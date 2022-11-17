a6132d23974d7cedd1a4b04a49f483c1bdb3b743
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-11-17 13:56:54 -0600

    Revert "all models loaded"
    This reverts commit d9bfa5f6b88704978fa7afba18b9f9b57cbf7b05.

--- gl.py
+++ gl.py
@@ -3,9 +3,6 @@ from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
 import pyrr
-import glfw
-import glfw.GLFW as GLFW_CONSTANTS
-from PIL import Image
 
 
 class Cube:
@@ -162,89 +159,6 @@ class App:
         pg.quit()
 
 
-class GraphicsEngine:
-
-    def __init__(self):
-
-        self.wood_texture = Material("gfx/wood.jpeg")
-        self.cube_mesh = Mesh("models/cube.obj")
-
-        # initialise opengl
-        glClearColor(0.1, 0.2, 0.2, 1)
-        self.shader = self.createShader(
-            "shaders/vertex.txt", "shaders/fragment.txt")
-        glUseProgram(self.shader)
-        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
-        glEnable(GL_DEPTH_TEST)
-
-        projection_transform = pyrr.matrix44.create_perspective_projection(
-            fovy=45, aspect=640/480,
-            near=0.1, far=10, dtype=np.float32
-        )
-        glUniformMatrix4fv(
-            glGetUniformLocation(self.shader, "projection"),
-            1, GL_FALSE, projection_transform
-        )
-        self.modelMatrixLocation = glGetUniformLocation(self.shader, "model")
-        self.viewMatrixLocation = glGetUniformLocation(self.shader, "view")
-
-    def createShader(self, vertexFilepath, fragmentFilepath):
-
-        with open(vertexFilepath, 'r') as f:
-            vertex_src = f.readlines()
-
-        with open(fragmentFilepath, 'r') as f:
-            fragment_src = f.readlines()
-
-        shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
-                                compileShader(fragment_src, GL_FRAGMENT_SHADER))
-
-        return shader
-
-    def render(self, scene):
-
-        # refresh screen
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glUseProgram(self.shader)
-
-        view_transform = pyrr.matrix44.create_look_at(
-            eye=scene.player.position,
-            target=scene.player.position + scene.player.forwards,
-            up=scene.player.up, dtype=np.float32
-        )
-        glUniformMatrix4fv(self.viewMatrixLocation, 1,
-                           GL_FALSE, view_transform)
-
-        self.wood_texture.use()
-        glBindVertexArray(self.cube_mesh.vao)
-        for cube in scene.cubes:
-            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
-            model_transform = pyrr.matrix44.multiply(
-                m1=model_transform,
-                m2=pyrr.matrix44.create_from_eulers(
-                    eulers=np.radians(cube.eulers), dtype=np.float32
-                )
-            )
-            model_transform = pyrr.matrix44.multiply(
-                m1=model_transform,
-                m2=pyrr.matrix44.create_from_translation(
-                    vec=np.array(cube.position), dtype=np.float32
-                )
-            )
-            glUniformMatrix4fv(self.modelMatrixLocation, 1,
-                               GL_FALSE, model_transform)
-            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
-
-        glFlush()
-
-    def quit(self):
-        self.cube_mesh.destroy()
-        self.wood_texture.destroy()
-        glDeleteProgram(self.shader)
-
-# returns the vertices
-
-
 class Mesh:
     def __init__(self, filename):
         # x, y, z, s, t, nx, ny, nz
@@ -264,7 +178,7 @@ class Mesh:
         # texture
         glEnableVertexAttribArray(1)
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE,
-                              32, ctypes.c_void_p(20))
+                              32, ctypes.c_void_p(12))
 
     def loadMesh(self, filename):
 
@@ -345,8 +259,6 @@ class Mesh:
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteBuffers(1, (self.vbo,))
 
-# applies texture to the OBJ
-
 
 class Material:
 
@@ -372,116 +284,25 @@ class Material:
         glDeleteTextures(1, (self.texture,))
 
 
-class Player:
+position = [0, -0.3, -3]
+eulers = [0, 0, 0]
 
-    def __init__(self, position):
+model = ""
+texture = ""
 
-        self.position = np.array(position, dtype=np.float32)
-        self.theta = 0
-        self.phi = 0
-        self.update_vectors()
+print("Modelos: \n[1] TIGER_TANK \n[2] Mask \n[3] Eyeball")
+model_option = int(input("Seleccione el modelo que desea cargar: \n"))
+if model_option == 1:
+    model = "models\Tiger2.obj"
+    texture = "models\Texture\Tiger.png"
 
-    def update_vectors(self):
+if model_option == 2:
+    model = "models\mask.obj"
+    texture = "models\Texture\Steve.png"
+    position = [0, -1, -4]
 
-        self.forwards = np.array(
-            [
-                np.cos(np.deg2rad(self.theta)) * np.cos(np.deg2rad(self.phi)),
-                np.sin(np.deg2rad(self.theta)) * np.cos(np.deg2rad(self.phi)),
-                np.sin(np.deg2rad(self.phi))
-            ]
-        )
-
-        globalUp = np.array([0, 0, 1], dtype=np.float32)
-
-        self.right = np.cross(self.forwards, globalUp)
-
-        self.up = np.cross(self.right, self.forwards)
-
-
-class Scene:
-
-    def __init__(self):
-
-        self.cubes = [
-            Cube(
-                position=[6, 0, 0],
-                eulers=[0, 0, 0]
-            )
-        ]
-
-        self.player = Player(position=[0, 0, 2])
-
-    def update(self, rate):
-
-        for cube in self.cubes:
-            cube.eulers[1] += 0.25 * rate
-            if cube.eulers[1] > 360:
-                cube.eulers[1] -= 360
-
-    def move_player(self, dPos):
-
-        dPos = np.array(dPos, dtype=np.float32)
-        self.player.position += dPos
-
-    def spin_player(self, dTheta, dPhi):
-
-        self.player.theta += dTheta
-        if self.player.theta > 360:
-            self.player.theta -= 360
-        elif self.player.theta < 0:
-            self.player.theta += 360
-
-        self.player.phi = min(
-            89, max(-89, self.player.phi + dPhi)
-        )
-
-        self.player.update_vectors()
-
-
-def menu():
-    position = [0, 0, -10]
-    eulers = [0, 0, 0]
-
-    model = ""
-    texture = ""
-
-    print(
-        "Modelos: \n[1] TIGER_TANK \n[2] Mask \n[3] Soldier \n[4] Gun\n[5] Hand")
-    model_option = int(input("Seleccione el modelo que desea cargar: \n"))
-    if model_option == 1:
-        model = "models\Tiger_.obj"
-        texture = "Textures\Tiger.png"
-        position = [0, -1, -8]
-
-    elif model_option == 2:
-        model = "models\mask.obj"
-        texture = "Textures\gold.jpg"
-        position = [0, -1, -4]
-
-    elif model_option == 3:
-        model = "models\\toysoldier.obj"
-        texture = "Textures\green.jpg"
-        position = [0, -0.5, -2]
-
-    elif model_option == 4:
-        model = "models\Gun.obj"
-        texture = "Textures\gray.jpg"
-        position = [0, 0, -1]
-
-    elif model_option == 5:
-        model = "models\hand_mod.obj"
-        texture = "Textures\gray.jpg"
-        position = [0, 0, -10]
-
-    App(model, texture, position, eulers)
-
-
-# position = [0, -0.2, -4]
-# eulers = [0, 0, 0]
-
-# model = "models\\toysoldier.obj"
-# texture = "models\Texture\gold.jpg"
-
-
-# myApp = App(model, texture, position, eulers)
-menu()
+if model_option == 3:
+    model = "models\eyeball.obj"
+    texture = "models\Texture\Eye_D.jpg"
+    position = [0, -0.5, -10]
+myApp = App(model, texture, position, eulers)
